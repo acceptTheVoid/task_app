b1b33b5f7fbd7fddd2d1a1c5f124286586797dbf
acceptTheVoid <arcad2004@gmail.com>
2022-10-19 05:26:12 +1000

    Добавил нотифаеры в dispose для высвобождения ресурсов

--- lib/controllers/notifier_controller.dart
+++ lib/controllers/notifier_controller.dart
@@ -10,7 +10,7 @@ class NotifierController extends StatefulWidget {
 
   static NotifierControllerData of(BuildContext context) {
     var data = context.dependOnInheritedWidgetOfExactType<_InheritedNotifierController>();
-    assert(data != null, 'Леее куда преш не видиш NotifierControllerData в дереве виджетов нет');
+    assert(data != null, 'Леее куда преш не видиш NotifierController в дереве виджетов нет');
     return data!.data;
   }
 
@@ -19,9 +19,25 @@ class NotifierController extends StatefulWidget {
 }
 
 class NotifierControllerData extends State<NotifierController> {
-  late TaskNotifier taskNotifier = TaskNotifier();
-  late AppBarNotifier appBarNotifier = AppBarNotifier();
-  late TitleNotifier titleNotifier = TitleNotifier();
+  late TaskNotifier taskNotifier;
+  late AppBarNotifier appBarNotifier;
+  late TitleNotifier titleNotifier;
+
+  @override
+  void initState() {
+    taskNotifier = TaskNotifier();
+    appBarNotifier = AppBarNotifier();
+    titleNotifier = TitleNotifier();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    titleNotifier.dispose();
+    appBarNotifier.dispose();
+    titleNotifier.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
