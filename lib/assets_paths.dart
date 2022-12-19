999586fe78801316e45b90e05ead60cb97fef116
acceptTheVoid <arcad2004@gmail.com>
2022-12-20 06:02:44 +1000

    убрал ненужную типизацию

--- lib/assets_paths.dart
+++ lib/assets_paths.dart
@@ -1,4 +1,4 @@
 abstract class AssetsPaths {
-  static const String todoListBackground = 'assets/todolist_background.svg';
-  static const String todoList = 'assets/todolist.svg';
+  static const todoListBackground = 'assets/todolist_background.svg';
+  static const todoList = 'assets/todolist.svg';
 }
